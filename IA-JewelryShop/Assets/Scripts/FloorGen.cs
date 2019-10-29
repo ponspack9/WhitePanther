a171a97688c7037ee8e7767d6e73a39dc88cb620
Oscar Pons <ospoga9@gmail.com>
2019-10-29 18:32:55 +0100

    Jewelry shop empty scene
    There's a script to test floors to chose the best one

---
+++ IA-JewelryShop/Assets/Scripts/FloorGen.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorGen : MonoBehaviour
+{
+    
+    public GameObject tile;
+    public Transform parent;
+    Vector3 position = new Vector3(0,0,0);
+    public int num_tiles_x = 5;
+    public int num_tiles_y = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < num_tiles_x; i++)
+        {
+            for (int j = 0; j < num_tiles_y; j++)
+            {
+                Instantiate(tile, position, Quaternion.identity,parent);
+                position.z += 5;
+            }
+            position.z = parent.position.z;
+            position.x += 5;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
