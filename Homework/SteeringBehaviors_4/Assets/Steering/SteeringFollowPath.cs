d8feb27f88b6d715417566e1475024d0ecc8b7d8
Oscar Pons <ospoga9@gmail.com>
2019-11-03 16:33:14 +0100

    Follow Path

--- Homework/SteeringBehaviors_4/Assets/Steering/SteeringFollowPath.cs
+++ Homework/SteeringBehaviors_4/Assets/Steering/SteeringFollowPath.cs
@@ -10,27 +10,34 @@ public class SteeringFollowPath : SteeringPriority {
 
     public float ratio_increment = 0.1f;
     public float min_distance = 1.0f;
-    float current_ratio = 0.0f;
+    float ratio = 0.0f;
 
     private float curveLength = 0.0f;
 
     private Vector3 closest_point = Vector3.zero;
 
-    BGCcMath path;
+    public BGCurve curve;
+    public BGCcMath math;
 
+    private float speed = 0;
 	// Use this for initialization
 	void Start () {
 		move = GetComponent<Move>();
 		seek = GetComponent<SteeringSeek>();
-        path = GetComponent<BansheeGz.BGSpline.Components.BGCcMath>();
+
+        //path = GetComponent<BansheeGz.BGSpline.Components.BGCcMath>();
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
+        ratio += ratio_increment * Time.deltaTime;
+
+        move.SetMovementVelocity(math.CalcPositionByDistanceRatio(ratio)-move.aim.transform.position);
+
         // TODO 2: Check if the tank is close enough to the desired point
         // If so, create a new point further ahead in the path
-	}
+    }
 
 	void OnDrawGizmosSelected() 
 	{
