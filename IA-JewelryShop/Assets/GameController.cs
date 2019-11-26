3814e65604a19372fdc27c8e5b7fe4e5aafb3a3d
Oscar Pons <ospoga9@gmail.com>
2019-11-26 23:27:36 +0100

    Organizing scene and making gamecontroller

---
+++ IA-JewelryShop/Assets/GameController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public int day = 0;
+    public int hour = 0;
+    public int minute = 0;
+
+    public float clients_rate = 0.5f;
+
+    [Header("-------- Read Only --------")]
+    public int number_shopkeepers = 1;
+    public int number_guards = 1;
+    public int number_clients = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
