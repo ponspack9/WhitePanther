888bb34206e2340c9a4f571f6f882be261ea2b11
Oscar Pons <ospoga9@gmail.com>
2019-11-28 00:05:58 +0100

    trying patrol

--- IA-JewelryShop/Assets/GameController.cs
+++ IA-JewelryShop/Assets/GameController.cs
@@ -9,9 +9,8 @@ public class GameController : MonoBehaviour
     public int day = 0;
     public int hour = 0;
     public int minute = 0;
-    private float time = 0.0f;
-
     public float time_rate = 0.5f;
+    private float time = 0.0f;
 
     [Header("Fame --------------------------------------------------------")]
     public float fame = 1.0f;
@@ -20,13 +19,20 @@ public class GameController : MonoBehaviour
     [Header("Shop --------------------------------------------------------")]
     public int number_shopkeepers = 1;
     public int number_guards = 1;
-    public int number_clients = 0;
+    public int number_costumers = 0;
+
+    [Header("Prefabs --------------------------------------------------------")]
+    public GameObject costumer_prefab;
+    public GameObject guard_prefab;
+    public GameObject shopkeeper_prefab;
 
     [Header("Canvas --------------------------------------------------------")]
     public Canvas canvas;
     public Text day_text;
     public Text hour_text;
     public Text minute_text;
+
+    private Vector3 costumer_start_pos = new Vector3(14, 0, 44);
 
     // Start is called before the first frame update
     void Start()
@@ -35,6 +41,20 @@ public class GameController : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        AdvanceTime();
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            AddCostumer();
+        }
+    }
+    private void AddCostumer()
+    {
+        Instantiate(costumer_prefab, costumer_start_pos, Quaternion.identity);
+        number_costumers++;
+    }
+    private void AdvanceTime()
     {
         time += Time.deltaTime * time_rate;
 
@@ -53,13 +73,9 @@ public class GameController : MonoBehaviour
                 day++;
             }
         }
-        UpdateCanvasTime();
-    }
-
-    void UpdateCanvasTime()
-    {
-        day_text.text = (day < 10 ) ? "0" + day.ToString() : day.ToString();
-        hour_text.text = (hour < 10) ? "0" + hour.ToString() : hour.ToString();
-        minute_text.text = (minute < 10) ? "0" + minute.ToString() : minute.ToString() ;
+        // Updating canvas
+        day_text.text    = (day < 10) ? "0" + day.ToString() : day.ToString();
+        hour_text.text   = (hour < 10) ? "0" + hour.ToString() : hour.ToString();
+        minute_text.text = (minute < 10) ? "0" + minute.ToString() : minute.ToString();
     }
 }
