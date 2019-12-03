a49dbc894ad3c0b1a75527aed3d8072732f78763
Oscar Pons <ospoga9@gmail.com>
2019-12-03 23:52:56 +0100

    Revert "Folder cleaning"
    This reverts commit adf2922112c85f2c2b41441d07e32c70585f2afe.

---
+++ IA-JewelryShop/Assets/Scripts/Steering/SteeringAlign.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class SteeringAlign : MonoBehaviour {
+
+	private float min_angle = 0.5f;
+	private float slow_angle = 5f;
+    public float rot_speed = 30.0f;
+
+	Move move;
+    private CostumerBehaviour cb;
+
+    // Use this for initialization
+    void Start () {
+        cb = GetComponent<CostumerBehaviour>();
+    }
+
+	// Update is called once per frame
+	void Update () 
+	{
+        //if (cb.arrived)
+        //{
+        //    //float angle = Mathf.Atan2(move.current_velocity.x, move.current_velocity.z);
+        //    float angle = Mathf.Atan2(cb.agent.destination.z, cb.agent.velocity.x);
+        //    transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.AngleAxis(Mathf.Rad2Deg * angle, Vector3.up), Time.deltaTime * rot_speed);
+
+        //}
+        //else
+        //{
+        //    // Orientation we are trying to match
+        //    float delta_angle = Vector3.SignedAngle(transform.forward, move.target.transform.forward, new Vector3(0.0f, 1.0f, 0.0f));
+
+        //    float diff_absolute = Mathf.Abs(delta_angle);
+
+        //    if (diff_absolute <= min_angle)
+        //    {
+        //        move.SetRotationVelocity(0.0f);
+        //        return;
+        //    }
+
+        //    float ideal_rotation_speed = move.max_rot_speed;
+
+        //    if (diff_absolute <= slow_angle)
+        //        ideal_rotation_speed *= (diff_absolute / slow_angle);
+
+        //    //float angular_acceleration = ideal_rotation_speed / time_to_accel;
+
+        //    //Invert rotation direction if the angle is negative
+        //    if (delta_angle < 0)
+        //        ideal_rotation_speed = -ideal_rotation_speed;
+
+        //    move.AccelerateRotation(Mathf.Clamp(ideal_rotation_speed, -move.max_rot_acceleration, move.max_rot_acceleration));
+        //}
+	}
+}
---
+++ IA-JewelryShop/Assets/Scripts/Steering/SteeringFollowPath.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+using BansheeGz.BGSpline.Components;
+using BansheeGz.BGSpline.Curve;
+
+
+[RequireComponent(typeof(BGCcMath))]
+public class SteeringFollowPath : SteeringPriority
+{
+
+    Move move;
+    SteeringArrive seek;
+
+    //public float ratio_increment = 0.1f;
+    //public float min_distance = 1.0f;
+    //float ratio = 0.0f;
+
+    //private float curveLength = 0.0f;
+
+    //private Vector3 closest_point = Vector3.zero;
+
+
+    public float distance = 0.0f;
+    public float distance_to_reach = 0.0f;
+
+    public BGCurve curve;
+    public BGCcMath math;
+    private Vector3 tangent;
+    // Use this for initialization
+    void Start()
+    {
+        move = GetComponent<Move>();
+        seek = GetComponent<SteeringArrive>();
+
+        distance_to_reach = math.GetDistance();
+
+        //move.target.transform.position = curve[1].PositionWorld;
+
+        //math = curve.GetComponent<BGCcMath>();
+
+        //path = GetComponent<BansheeGz.BGSpline.Components.BGCcMath>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        distance += move.max_mov_speed * Time.deltaTime;
+
+        if (distance > distance_to_reach) distance = 0.0f;
+
+        
+
+        //move.target.transform.position = math.CalcPositionAndTangentByDistance(distance, out tangent);
+
+        move.target.transform.SetPositionAndRotation(
+            math.CalcPositionAndTangentByDistance(distance, out tangent),
+            Quaternion.LookRotation(tangent)
+            );
+
+        //ratio += ratio_increment * Time.deltaTime;
+
+        //int section = math.CalcSectionIndexByDistanceRatio(ratio);
+
+        //seek.Steer(curve.Points[section].PositionWorld);
+
+        //move.SetMovementVelocity(math.CalcPositionByDistanceRatio(ratio) - move.aim.transform.position);
+
+        // TODO 2: Check if the tank is close enough to the desired point
+        // If so, create a new point further ahead in the path
+    }
+
+    void OnDrawGizmosSelected()
+    {
+
+        if (isActiveAndEnabled)
+        {
+            // Display the explosion radius when selected
+            Gizmos.color = Color.green;
+            // Useful if you draw a sphere were on the closest point to the path
+        }
+
+    }
+}
