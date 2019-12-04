e1f611c8fb531bd7da45ce45a2d912df19f828d0
Oscar Pons <ospoga9@gmail.com>
2019-12-04 21:46:11 +0100

    Client Simple Behaviour

---
+++ IA-JewelryShop/Assets/Client.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Client : MonoBehaviour
+{
+    public bool is_buying = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
